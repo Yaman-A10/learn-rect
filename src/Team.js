58b7d71bb0d0e39d672d91be650a587dbacb82c4
Lars Gersmann <lars.gersmann@gmail.com>
2021-11-29 11:11:25 +0100

    fix: made css working

--- src/Team.js
+++ src/Team.js
@@ -1,12 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import TeamMember from "./TeamMember";
-function Team(){
-    return(
-        <div className='row'>
-            <TeamMember name='Yaman'/>
-            <TeamMember name='wisaam'/>
-            <TeamMember name='Mumen'/>
-        </div>
-    )
+
+let counter = 1;
+
+function Team() {
+  const [teamMembers, setTeamMembers] = useState([
+    "Yaman",
+    "wisaam",
+    "Mumen",
+    "Lars",
+  ]);
+
+  const addTeamMember = () => {
+    setTeamMembers([...teamMembers, `Member ${counter++}`]);
+  };
+
+  return (
+    <div>
+      <div className="row">
+        {teamMembers.map((teamMember, index) => (
+          <TeamMember key={index} name={teamMember} />
+        ))}
+      </div>
+      <button onClick={addTeamMember}>add team member</button>
+    </div>
+  );
 }
-export default Team
+export default Team;
